70710694476141557dc69acf65aea4576a744c4d
Moamin <moaminabuewaida@Moamins-MacBook-Air.local>
2022-03-08 10:35:39 +0100

    1st commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Canvas} from './Canvas'
+import {ClearCanvasButton} from './ClearCanvasButton'
+import './App.css';
+
+function App() {
+
+  return (
+      <div>
+        <Canvas />
+        <ClearCanvasButton />
+      </div>
+  );
+}
+
+export default App;
